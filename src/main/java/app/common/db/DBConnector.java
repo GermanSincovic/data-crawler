08b7dca11e9b2478625e1432d73f035e204aba44
Ivan_Didyk <german.sincovic.92@gmail.com>
2022-01-16 18:57:45 +0200

    Refactoring

---
+++ src/main/java/app/common/db/DBConnector.java
@@ -0,0 +1,70 @@
+package app.common.db;
+
+import com.mysql.jdbc.Driver;
+import lombok.extern.slf4j.Slf4j;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.statement.SqlLogger;
+import org.jdbi.v3.core.statement.StatementContext;
+import org.jdbi.v3.jodatime2.JodaTimePlugin;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.time.temporal.ChronoUnit;
+
+import static java.lang.System.getProperty;
+
+@Slf4j
+public class DBConnector {
+
+  private static final String DATABASE_URL = getProperty("db.url");
+  private static final String DATABASE_USER = getProperty("db.user");
+  private static final String DATABASE_PASS = getProperty("db.password");
+
+  private static DBConnector instance;
+
+  private Jdbi jdbi;
+
+  private DBConnector() {
+    try {
+      Driver driver = new Driver();
+      DriverManager.registerDriver(driver);
+      Connection connection =
+              DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASS);
+      jdbi = Jdbi.create(connection);
+      jdbi.installPlugin(new SqlObjectPlugin());
+      jdbi.installPlugin(new JodaTimePlugin());
+      jdbi.setSqlLogger(
+              new SqlLogger() {
+                @Override
+                public void logAfterExecution(StatementContext context) {
+                  log.info(
+                          "\n{}\nQUERY STATEMENT: [{}]\nQUERY PARAMS: [{}]\nEXECUTION TIME: [{}ms]\n{}",
+                          "================================================",
+                          context.getParsedSql().getSql(),
+                          context.getBinding().toString(),
+                          context.getElapsedTime(ChronoUnit.MILLIS),
+                          "================================================");
+                }
+              });
+    } catch (SQLException e) {
+      log.error(e.getMessage());
+    }
+  }
+
+  public static DBConnector getInstance() {
+    if (instance == null) {
+      instance = new DBConnector();
+    }
+    return instance;
+  }
+
+  public <T> T with(Class<T> clazz) {
+    return jdbi.onDemand(clazz);
+  }
+
+  public void execute(String q) {
+    jdbi.withHandle(h -> h.execute(q));
+  }
+}
--- src/test/java/MonoTest.java
+++ src/test/java/MonoTest.java
@@ -1,29 +1,30 @@
+import app.common.db.DBConnector;
+import app.mono.api.MonoApiClient;
 import app.mono.model.BankCurrency;
-import app.utils.JsonUtil;
 import org.testng.annotations.Test;
 
 public class MonoTest {
 
   @Test
   public static void currency(){
-    //    Currency[] currencies = new MonoApiClient().getBankCurrency();
-    String rawBody =
-            "[\n"
-                    + "  {\n"
-                    + "    \"currencyCodeA\": 840,\n"
-                    + "    \"currencyCodeB\": 980,\n"
-                    + "    \"date\": 1552392228,\n"
-                    + "    \"rateSell\": 27,\n"
-                    + "    \"rateBuy\": 27.2,\n"
-                    + "    \"rateCross\": 27.1\n"
-                    + "  }\n"
-                    + "]";
-    BankCurrency[] currencies = JsonUtil.toObject(rawBody, BankCurrency[].class);
-    for (BankCurrency currency : currencies) {
-      System.out.println(currency.toString());
-    }
+//    BankCurrency[] currencies = new MonoApiClient().getBankCurrency();
+//    String rawBody =
+//            "[\n"
+//                    + "  {\n"
+//                    + "    \"currencyCodeA\": 840,\n"
+//                    + "    \"currencyCodeB\": 980,\n"
+//                    + "    \"date\": 1552392228,\n"
+//                    + "    \"rateSell\": 27,\n"
+//                    + "    \"rateBuy\": 27.2,\n"
+//                    + "    \"rateCross\": 27.1\n"
+//                    + "  }\n"
+//                    + "]";
 
     System.out.println(System.getProperty("mono.api.token"));
+    System.out.println(System.getProperty("db.url"));
+    System.out.println(System.getProperty("db.user"));
+    System.out.println(System.getProperty("db.password"));
+    System.out.println(DBConnector.getInstance());
   }
 
 }
