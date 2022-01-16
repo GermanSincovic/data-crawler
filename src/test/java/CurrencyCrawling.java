2acfd1054362198cf68da437eb858a38346e6b99
Ivan_Didyk <german.sincovic.92@gmail.com>
2022-01-16 21:08:04 +0200

    Refactoring

--- src/test/java/CurrencyCrawling.java
+++ src/test/java/CurrencyCrawling.java
@@ -2,6 +2,7 @@ import app.common.db.model.DBCurrency;
 import app.mono.CurrencyCodeISO4217Enum;
 import app.mono.api.MonoApiClient;
 import app.mono.model.BankCurrency;
+import app.utils.JsonUtil;
 import org.joda.time.DateTime;
 import org.testng.annotations.Test;
 
@@ -13,6 +14,26 @@ public class CurrencyCrawling {
   public static void main(){
     MonoApiClient monoApiClient = new MonoApiClient();
     BankCurrency[] bankCurrencies = monoApiClient.getBankCurrency();
+//    String data = "[\n" +
+//            "    {\n" +
+//            "      \"currencyCodeA\": 840,\n" +
+//            "            \"currencyCodeB\": 980,\n" +
+//            "            \"date\": 1552392228,\n" +
+//            "            \"rateSell\": 27,\n" +
+//            "            \"rateBuy\": 27.2,\n" +
+//            "            \"rateCross\": 27.1\n" +
+//            "    },\n" +
+//            "    {\n" +
+//            "      \"currencyCodeA\": 978,\n" +
+//            "            \"currencyCodeB\": 980,\n" +
+//            "            \"date\": 1552392228,\n" +
+//            "            \"rateSell\": 28,\n" +
+//            "            \"rateBuy\": 28.2,\n" +
+//            "            \"rateCross\": 28.1\n" +
+//            "    }\n" +
+//            "]";
+//    BankCurrency[] bankCurrencies = JsonUtil.toObject(data, BankCurrency[].class);
+
     BankCurrency bankCurrencyUSD = getByCodes(CurrencyCodeISO4217Enum.USD, CurrencyCodeISO4217Enum.UAH, bankCurrencies);
     BankCurrency bankCurrencyEUR = getByCodes(CurrencyCodeISO4217Enum.EUR, CurrencyCodeISO4217Enum.UAH, bankCurrencies);
 
